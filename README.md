15f58087b50dad509e3b51ae19e085e65f249490
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-02 10:36:06 +0900

    pug

--- README.md
+++ README.md
@@ -5,11 +5,16 @@
 /login -> 로그인
 /search -> 검색기능
 
+/users/:id -> See User
+/users/logout -> 로그 아웃
 /users/edit -> 사용자 편집
-/users/delete -> 사용자 삭제?
+/users/delete -> 사용자 삭제
 
+/videos/:id -> 영상보기
 /videos/watch -> 영상 시청
-/video/edit -> 영상 편집
-/video/delete -> 영상 삭제
+/video/:id/edit -> 영상 편집
+/video/:id/delete -> 영상 삭제
+/video/upload -> 영상 업로드
+
 /video/comments -> 영상 댓글
 /video/comments/delete -> 영상 댓글 삭제
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,8 @@
-export const trendingVideos =(req,res) =>res.send("Home Page Videos");
-export const search=(req,res)=>res.send("Search");
+export const trendingVideos =(req,res) =>res.render("home");
+export const search=(req,res)=>res.render("watc");
 
-export const see=(req,res)=>{
-   return res.send("Watch")
-};
-export const edit=(req,res)=> {
-    return res.send("Edit")
-};
+export const see=(req,res)=>res.render("watch");
+export const edit=(req,res)=>res.render("edit");
 export const upload=(req,res)=>res.send("Upload");
 export const deleteVideo=(req,res)=>{
      return res.send("Delete Video");
