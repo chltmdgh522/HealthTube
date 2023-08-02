645c51774171037a87e80773374d594541d55b2a
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-02 13:26:24 +0900

    iterator, mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,40 @@
-export const trendingVideos =(req,res) =>res.render("home");
+const fakeUser={
+    username:"최승호",
+    loggedIn:true, 
+}
+
+export const trendingVideos =(req,res) =>{
+    const videos=[
+        {
+            title:"a",
+            rating:5,
+            comments:2,
+            createdAt:"2분전",
+            views:59,
+            id:1,
+        },
+        {
+            title:"b",
+            rating:5,
+            comments:2,
+            createdAt:"2분전",
+            views:59,
+            id:1,
+        },
+        {
+            title:"c",
+            rating:5,
+            comments:2,
+            createdAt:"2분전",
+            views:59,
+            id:1,
+        },
+    ];
+    res.render("home",{pageTitle:"Home",fakeUser:fakeUser,videos});
+}
 export const search=(req,res)=>res.render("watc");
 
-export const see=(req,res)=>res.render("watch");
+export const see=(req,res)=>res.render("watch",{pageTitle:"영상 시청"});
 export const edit=(req,res)=>res.render("edit");
 export const upload=(req,res)=>res.send("Upload");
 export const deleteVideo=(req,res)=>{
