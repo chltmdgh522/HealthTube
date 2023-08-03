32d0c137cb5c437fd3bed1e73a1953c3c07c5be3
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-03 11:26:50 +0900

    DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,41 +1,52 @@
-const fakeUser={
-    username:"최승호",
-    loggedIn:true, 
-}
-
+const fakeUser = {
+    username: "최승호",
+    loggedIn: false,
+  };
+let videos=[
+    {
+        title:"First Video",
+        rating:5,
+        comments:2,
+        createdAt:"2분전",
+        views:1,
+        id:1,
+    },
+    {
+        title:"Second Video",
+        rating:5,
+        comments:2,
+        createdAt:"2분전",
+        views:59,
+        id:2,
+    },
+    {
+        title:"Thrid Video",
+        rating:5,
+        comments:2,
+        createdAt:"2분전",
+        views:59,
+        id:3,
+    },
+];
 export const trendingVideos =(req,res) =>{
-    const videos=[
-        {
-            title:"a",
-            rating:5,
-            comments:2,
-            createdAt:"2분전",
-            views:59,
-            id:1,
-        },
-        {
-            title:"b",
-            rating:5,
-            comments:2,
-            createdAt:"2분전",
-            views:59,
-            id:1,
-        },
-        {
-            title:"c",
-            rating:5,
-            comments:2,
-            createdAt:"2분전",
-            views:59,
-            id:1,
-        },
-    ];
     res.render("home",{pageTitle:"Home",fakeUser:fakeUser,videos});
 }
-export const search=(req,res)=>res.render("watc");
+export const search=(req,res)=>{
 
-export const see=(req,res)=>res.render("watch",{pageTitle:"영상 시청"});
-export const edit=(req,res)=>res.render("edit");
+};
+export const watch=(req,res)=>{
+    const id= req.params.id;
+    const video =videos[id-1];
+    return res.render("watch",{pageTitle:`영상 시청 ${video.title}`,fakeUser,video});
+}
+export const getEdit=(req,res)=>{
+    const id= req.params.id;
+    const video =videos[id-1];
+    return res.render("edit",{pageTitle:`${video.title}의 영상을 편집해보세요`,video,fakeUser:fakeUser});
+}
+export const postEdit=(req,res)=>{}
+
+
 export const upload=(req,res)=>res.send("Upload");
 export const deleteVideo=(req,res)=>{
      return res.send("Delete Video");
