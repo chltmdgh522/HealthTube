5c2b6fb901df094403e22655afde8dc6ca5d1268
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-10 21:43:59 +0900

    깃허브 로그인

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,6 +60,21 @@ export const postLogin=async(req,res)=>{
     return res.redirect("/");
 }
 
+export const startGithubLogin=(req,res)=>{
+    const baseUrl="https://github.com/login/oauth/authorize";
+    const config={
+        client_id:"7252c77043ef4734b0b9",
+        allow_signup:false,
+        scope:"read:user user:email",
+    };
+    const params=new URLSearchParams(config).toString();
+    const finalUrl=`${baseUrl}?${params}`;
+    return res.redirect(finalUrl);
+}
+
+export const finishGithubLogin=(req,res)=>{
+    
+}
 
 export const edit=(req,res)=>res.send("edit");
 export const remove=(req,res)=>res.send("Delete User" );
