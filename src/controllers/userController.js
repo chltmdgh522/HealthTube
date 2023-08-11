e056edae0f7aab925fdb6931106744bfbb0fff70
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-11 14:59:45 +0900

    로그아웃

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -42,7 +42,7 @@ export const getLogin=(req,res)=>{
 }
 export const postLogin=async(req,res)=>{
     const{username,password}=req.body;
-    const exists=await User.exists({username})
+    const exists=await User.exists({username, socialOnly:false})
     if(!exists){
         return res.status(400).render("login",{pageTitle:"로그인",errorMessage:"존재하지 않은 아이디입니다."});
     }
@@ -114,32 +114,32 @@ export const finishGithubLogin=async(req,res)=>{
         if (!emailObj) {
           return res.redirect("/login");
         }
-        const existingUser=await User.findOne({email:emailObj.email});
-        if(existingUser){
-            req.session.loggedIn=true;
-            req.session.user=existingUser;
-            return res.redirect("/");
-        }else {
-            const user = await User.create({
-              name: userData.name,
-              username: userData.login,
-              email: emailObj.email,
-              password: "",
-              socialOnly: true,
-              location: userData.location,
-            });
-            req.session.loggedIn = true;
-            req.session.user = user;
-            return res.redirect("/");
-          }
+        let user = await User.findOne({ email: emailObj.email });
+        if (!user) {
+          user = await User.create({
+            avatarUrl: userData.avatar_url,
+            name: userData.name,
+            username: userData.login,
+            email: emailObj.email,
+            password: "",
+            socialOnly: true,
+            location: userData.location,
+          });
+        }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
       } else {
         return res.redirect("/login");
       }
-  
-};
+    };
+    export const logout = (req, res) => {
+        req.session.destroy();
+        return res.redirect("/");
+      };
 
 export const edit=(req,res)=>res.send("edit");
 export const remove=(req,res)=>res.send("Delete User" );
 
-export const logout=(req,res)=>res.send("Log Out" );
+
 export const see=(req,res)=>res.send("See User" );
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -10,4 +10,5 @@ rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search",search);
 
+
 export default rootRouter;
