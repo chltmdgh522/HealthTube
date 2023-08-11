e903ebfe0c532e188fefc51aeae6f68f91465622
seung_ho_choi.s <chltmdgh517@naver.com>
2023-08-11 14:38:03 +0900

    최종 깃허브 로그인

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema= new mongoose.Schema({
-    email: {type:String, required: true, unique: true},
-    username: {type:String, required: true, unique: true},
-    password: {type:String, required: true},
-    email: {type:String, required: true},
-    name:{type:String, required:true},
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    socialOnly: { type: Boolean, default: false },
+    username: { type: String, required: true, unique: true },
+    password: { type: String },
+    name: { type: String, required: true },
     location: String,
-})
+  });
 
 userSchema.pre("save",async function(){
     this.password=await bcrypt.hash(this.password,5);
-} )
+} );
 
 const User=mongoose.model("User",userSchema);
 
