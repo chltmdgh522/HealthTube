dad07404d311b878f7e26a4a061fe7f4771f266c
seung_ho_choi.s <74850409+chltmdgh522@users.noreply.github.com>
2023-12-20 22:38:18 +0900

    db.js 업데이트

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ mongoose.connect(process.env.DB_URL, //환경변수에서 가져옴
 useUnifiedTopology:true,}
 );
 
+
 const db= mongoose.connection;
 const handleOpen=() => console.log("데이터베이스 연결됨");
 const handleError=(error)=> console.log("에러남", error);
